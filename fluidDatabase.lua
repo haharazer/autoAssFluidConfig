8682b54c9b344915e84243565cc614cfa8f1c75c
haharazer <hanpeng1986@gmail.com>
2022-05-26 17:32:15 +0800

    Update fluidDatabase.lua

--- fluidDatabase.lua
+++ fluidDatabase.lua
@@ -3,7 +3,7 @@ fluidDatabaseMetaTable = {}
 
 fluidDatabase['熔融量子单元'] = '熔融量子锭'
 fluidDatabase['熔融Naquadria单元'] = '熔融超能硅岩'
-fluidDatabase['氖单元'] = '
+fluidDatabase['氖单元'] = 'Neon'
 fluidDatabase['fluid.molten.solderingalloy'] = 'gt.metaitem.99.314.name'
 fluidDatabase['fluid.lubricant'] = 'gt.metaitem.01.30724.name'
 fluidDatabase['fluid.molten.styrenebutadienerubber'] = 'gt.metaitem.99.635.name'
